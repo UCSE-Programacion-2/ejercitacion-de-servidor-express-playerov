c5c80248e0c177ac8a4c9e72b3f0f06a3361d887
playerov <franmendoza2323@gmail.com>
2026-06-03 23:17:22 -0300

    homework

--- src/server.js
+++ src/server.js
@@ -15,5 +15,28 @@ const usuarios = [
 ];
 
 // TODO: Define tus rutas aquí
+app.get('/', (req, res) => {
+  res.send('Bienvenid@s a nuestro servidor Express!');
+});
 
+app.get('/productos', (req, res) => {
+  const { categoria } = req.query;
+  if (categoria) {
+    res.json(productos.filter((p) => p.categoria === categoria));
+  } else {
+    res.json(productos);
+  }
+});
+
+app.get('/usuarios/:id', (req, res) => {
+  const usuario = usuarios.find((u) => u.id === parseInt(req.params.id));
+  if (!usuario) {
+    return res.status(404).send('Usuario no encontrado.');
+  }
+  res.json(usuario);
+});
+
+app.use((req, res) => {
+  res.status(404).send('No se ha encontrado la ruta ingresada.');
+});
 module.exports = app;
